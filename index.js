1e8b5154ea68a53760aeb936333d49e3667102f4
mzb527 <mzb527@gmail.com>
2025-03-10 18:15:59 -0400

    Lab
    Lab uploaded

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 //Write your code here
 
+// Declare and Initialize an Attendee Object
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
+
+// Function to Log Attendee Name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+// Function to Log Ticket Price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+// Function to Update Ticket Type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+// Function to Update Ticket Price
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+}
+
+// Function to Remove the Event Property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Function to Add a Checked-in Property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
